1de292e4241d6e4a13898ad323eb144c52ed7520
tranleanhthe <tranleanhthe@gmail.com>
2017-11-26 01:46:43 +0700

    update

--- src/server/routes/upload.routes.js
+++ src/server/routes/upload.routes.js
@@ -2,36 +2,37 @@ var router = require('express').Router();
 var http = require('http');
 var path = require('path');
 var fs = require("fs");
-
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
+var ip = require('ip');
 module.exports = function () {
-  router.post("/", update);
+  router.post("/", multipartMiddleware, update);
+  router.get('/', function (req, res) {
+    res.render('index');
+  });
 
   function update(req, res, next) {
-    var dest, fileName, l, tmpPath;
-    tmpPath = req.files.upload.path;
-    l = tmpPath.split('/').length;
-    fileName = tmpPath.split('/')[l - 1] + "_" + req.files.upload.name;
-    dest = __dirname + "/public/uploads/" + fileName;
+    var fs = require('fs');
+
     fs.readFile(req.files.upload.path, function (err, data) {
-      if (err) {
-        console.log('this is bug');
-        return;
-      }
-      fs.writeFile(dest, data, function (err) {
-        var html;
-        if (err) {
-          console.log('this is bug');
-          return;
+      var newPath = __dirname + '/../public/uploads/' + req.files.upload.name;
+      fs.writeFile(newPath, data, function (err) {
+        if (err) console.log({
+          err: err
+        });
+        else {
+          console.log(ip.address());
+          html = "";
+          html += "<script type='text/javascript'>";
+          html += "    var funcNum = " + req.query.CKEditorFuncNum + ";";
+          html += "    var url     = \"http://" + ip.address() + ":3002/src/server/public/uploads/" + req.files.upload.name + "\";";
+          html += "    var message = \"Uploaded file successfully\";";
+          html += "";
+          html += "    window.parent.CKEDITOR.tools.callFunction(funcNum, url, message);";
+          html += "</script>";
+
+          res.send(html);
         }
-        html = "";
-        html += "<script type='text/javascript'>";
-        html += "    var funcNum = " + req.query.CKEditorFuncNum + ";";
-        html += "    var url     = \"/uploads/" + fileName + "\";";
-        html += "    var message = \"Uploaded file successfully\";";
-        html += "";
-        html += "    window.parent.CKEDITOR.tools.callFunction(funcNum, url, message);";
-        html += "</script>";
-        res.stauts(200).send(html);
       });
     });
   }
